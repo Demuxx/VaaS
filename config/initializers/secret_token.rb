9379006f2b484191d19d2342beae51e4b23fbe9c
Michael Carlson <michael.carlson@MCARLSON-05.local>
2013-10-04 11:53:02 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VaasGit::Application.config.secret_key_base = '969336f56f6027b6803cbd0c07a5fcc94b1b3f7018211ace0ffb1c47e7abed28c04f12680b16548b3d3ce7389387fa194129a372544de94fac1bc84040f4d760'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VaasGit::Application.config.session_store :cookie_store, key: '_vaas_git_session'
